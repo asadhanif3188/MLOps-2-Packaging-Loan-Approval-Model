abb0f507903620daf75ca7891034503ddcf93217
Asad Hanif <asadhanif3188@gmail.com>
2024-06-28 12:18:29 +0500

    Code added in Config Module

---
+++ packaging-loan-model/prediction_model/config/config.py
@@ -0,0 +1,54 @@
+import pathlib 
+import os
+import prediction_model 
+
+PACKAGE_ROOT = pathlib.Path(prediction_model.__file__).resolve().parent
+
+DATA_PATH = os.path.join(PACKAGE_ROOT, 'datasets')
+
+TRAIN_FILE = 'train.csv'
+TEST_FILE = 'test.csv'
+
+MODEL_NAME = 'loan-classification.pkl'
+SAVE_MODEL_PATH = os.path.join(PACKAGE_ROOT, 'trained_models')
+
+TARGET = 'Loan_Status'
+
+FEATURES = ['Gender', 
+            'Married', 
+            'Dependents', 
+            'Education', 
+            'Self_Employed',
+            'ApplicantIncome', 
+            'LoanAmount', 
+            'Loan_Amount_Term', 
+            'Credit_History',
+            'Property_Area']
+
+NUM_FEATURES = ['ApplicantIncome', 
+                'LoanAmount', 
+                'Loan_Amount_Term']
+
+CAT_FEATURES = ['Gender',
+                'Married',
+                'Dependents',
+                'Education',
+                'Self_Employed',
+                'Credit_History',
+                'Property_Area']
+
+# for this problem, our cat_features need to be encoded 
+FEATURES_TO_ENCODE = ['Gender',
+                'Married',
+                'Dependents',
+                'Education',
+                'Self_Employed',
+                'Credit_History',
+                'Property_Area']
+
+FEATURE_TO_MODIFY = ['ApplicantIncome']
+FEATURE_TO_ADD = 'CoapplicantIncome'
+
+DROP_FEATURES = ['CoapplicantIncome']
+
+LOG_FEATURES = ['ApplicantIncome', 'LoanAmount'] # taking log of numerical columns
